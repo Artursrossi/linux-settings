e5861ac1d6109e262281f337b338c9b11ca9da4b
Artur Schincariol Rossi <artursrossi4@gmail.com>
2025-12-08 21:33:44 -0300

    feat: Shell script for install Flatpak Apps

---
+++ src/install-flatpack-apps.sh
@@ -0,0 +1,52 @@
+# Install Apps using Flatpak
+# List of Flatpak apps to install
+# - Discord
+# - Brave Browser
+# - Flameshot
+# - Postman
+# - Spotify
+# - RedisInsight
+# - MongoDB Compass
+# - DBeaver Community Edition
+# - OBS Studio
+
+#!/bin/bash
+set -e
+
+###################################################################################################################################
+
+# Define Apps origin from Flathub
+APPS=(
+    com.discordapp.Discord
+    com.brave.Browser
+    org.flameshot.Flameshot
+    com.getpostman.Postman
+    com.spotify.Client
+    com.redis.RedisInsight
+    com.mongodb.Compass
+    io.dbeaver.DBeaverCommunity
+    com.obsproject.Studio
+)
+
+###################################################################################################################################
+
+# Add Flathub repository if not already added
+echo "🔄 Updating flathub..."
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+###################################################################################################################################
+
+# Install Apps
+echo "📦 Installing Flatpak apps..."
+for APP in "${APPS[@]}"; do
+    if flatpak info "$APP" &>/dev/null; then
+        echo "✔️ $APP already installed. Skipping."
+    else
+        echo "⬇️ Installing: $APP"
+        flatpak install -y flathub "$APP"
+    fi
+done
+
+###################################################################################################################################
+
+echo "🚀 Done!"
